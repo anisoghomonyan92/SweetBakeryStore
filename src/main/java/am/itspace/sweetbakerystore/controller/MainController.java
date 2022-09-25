b8e16dca6ddee6c9a4fb1a35b1a61e65ee7963f5
Ani Soghomonyan <anuliks@yahoo.com>
2022-09-25 23:57:05 +0400

    scc

--- src/main/java/am/itspace/sweetbakerystore/controller/MainController.java
+++ src/main/java/am/itspace/sweetbakerystore/controller/MainController.java
@@ -1,2 +1,13 @@
-package am.itspace.sweetbakerystore.controller;public class MainController {
+package am.itspace.sweetbakerystore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping(value = "/")
+    public String massage() {
+        return "index";
+    }
 }
