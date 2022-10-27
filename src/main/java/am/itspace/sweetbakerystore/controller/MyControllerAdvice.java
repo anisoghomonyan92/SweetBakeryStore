f8b24944ce6c84beff6eacacfad33c540a055e08
Ani Soghomonyan <anuliks@yahoo.com>
2022-10-28 01:00:57 +0400

    Set active class in admin panel, Fix style(edit-account).

--- src/main/java/am/itspace/sweetbakerystore/controller/MyControllerAdvice.java
+++ src/main/java/am/itspace/sweetbakerystore/controller/MyControllerAdvice.java
@@ -7,11 +7,19 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import javax.servlet.http.HttpServletRequest;
+
 @ControllerAdvice
 public class MyControllerAdvice {
 
     @ModelAttribute(name = "currentUser")
     public User currentUser(@AuthenticationPrincipal CurrentUser currentUser) {
         return currentUser == null ? null : currentUser.getUser();
+    }
+
+    @ModelAttribute(name = "currentUrl")
+    public String currentUrl(HttpServletRequest request) {
+        return request.getRequestURI();
+
     }
 }
